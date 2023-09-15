a459bdecc68d8f3d4140758a58e0c7a0fb171d68
Cristian Estrada <cdestradab@gmail.com>
2023-09-15 00:57:09 -0500

    Paginación

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,9 +6,9 @@ import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -24,7 +24,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DatosListadoMedico> listadoMedicos(){
-        return medicoRepository.findAll().stream().map(DatosListadoMedico::new).toList();
+    public Page<DatosListadoMedico> listadoMedicos(Pageable paginacion){
+        return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
     }
 }
