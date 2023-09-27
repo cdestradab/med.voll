3388901ece4a6a215d33a46bab3c0fc232f480da
Cristian Estrada <cdestradab@gmail.com>
2023-09-27 00:45:16 -0500

    Personalización de Token

--- src/main/java/med/voll/api/controller/AutenticacionController.java
+++ src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -2,6 +2,7 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuarios.DatosAutenticacionUsuario;
+import med.voll.api.domain.usuarios.Usuario;
 import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -28,9 +29,9 @@ public class AutenticacionController {
 
         Authentication authToken = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(),
                 datosAutenticacionUsuario.clave());
-        authenticationManager.authenticate(authToken);
+        var usuarioAutenticado = authenticationManager.authenticate(authToken);
 
-        var JWTtoken = tokenService.generarToken();
+        var JWTtoken = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
 
         //Fines didacticos:
         return ResponseEntity.ok(JWTtoken);
--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,21 +3,32 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.domain.usuarios.Usuario;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
-    public String generarToken() {
+    public String generarToken(Usuario usuario) {
         try {
             Algorithm algorithm = Algorithm.HMAC256("12345");
             String token = JWT.create()
                     .withIssuer("voll med")
-                    .withSubject("cdeb")
+                    .withSubject(usuario.getLogin())
+                    .withClaim("id", usuario.getId())
+                    .withExpiresAt(generarFechaExpiracion())
                     .sign(algorithm);
 
             return token;
         } catch (JWTCreationException exception){
             throw new RuntimeException();
         }
+    }
+
+    private Instant generarFechaExpiracion(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));
     }
 }
