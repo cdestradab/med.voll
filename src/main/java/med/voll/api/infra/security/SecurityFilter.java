6f3162585f5b65f06c982dc54501da2fcb359381
Cristian Estrada <cdestradab@gmail.com>
2023-09-27 23:06:53 -0500

    Validando el Token

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,6 +12,12 @@ import java.io.IOException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+    //Lo inmeditamente siguiente es mejor hacerlo a nivel de constructor
+    //Pero se deja así con fines didácticos
+    @Autowired
+    private TokenService tokenService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // Obtener token del header
@@ -20,6 +27,9 @@ public class SecurityFilter extends OncePerRequestFilter {
         }
         token = token.replace("Bearer ", "");
         System.out.println(token);
+
+        System.out.println(tokenService.getSubject(token));
+
         filterChain.doFilter(request, response);
     }
 }
--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -1,8 +1,11 @@
 package med.voll.api.infra.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import med.voll.api.domain.usuarios.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -30,6 +33,29 @@ public class TokenService {
         } catch (JWTCreationException exception){
             throw new RuntimeException();
         }
+    }
+
+    public String getSubject(String token) {
+        DecodedJWT verifier = null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            verifier = JWT.require(algorithm)
+                    .withIssuer("voll med")
+                    .build()
+                    .verify(token);
+
+        } catch (JWTVerificationException exception){
+            System.out.println(exception.toString());
+        }
+
+        //assert verifier != null; //Valida que el verifier no sea null
+        //Pero en este caso es mejor usar un if, pues assert es para pruebas unitarias.
+
+        if (verifier.getSubject() == null){
+            throw new RuntimeException("Verifier inválido");
+        }
+
+        return verifier.getSubject();
     }
 
     private Instant generarFechaExpiracion(){
