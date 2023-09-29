3c581a214671b26833d29ca4333d41169871f55a
Cristian Estrada <cdestradab@gmail.com>
2023-09-29 01:06:00 -0500

    Validación de integridad de ID ingresada de paciente y médico en solicitud de consulta

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultaService.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultaService.java
@@ -4,6 +4,7 @@ import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.Paciente;
 import med.voll.api.domain.paciente.PacienteRepository;
+import med.voll.api.infra.errores.ValidacionDeIntegridad;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,12 +22,26 @@ public class AgendaDeConsultaService {
 
     public void agendar(DatosAgendarConsulta datosAgendarConsulta){
 
+        if(pacienteRepository.findById(datosAgendarConsulta.idPaciente()).isPresent()){
+            throw new ValidacionDeIntegridad("ID de paciente no fué encontrado");
+        };
+
+        if(datosAgendarConsulta.idMedico() != null && medicoRepository.existsById(datosAgendarConsulta.idMedico())){
+            throw new ValidacionDeIntegridad("ID de médico no fué encontrado");
+        }
+
+
+
         var paciente = pacienteRepository.findById(datosAgendarConsulta.idPaciente()).get();
 
-        var medico = medicoRepository.findById(datosAgendarConsulta.idMedico()).get();
+        var medico = seleccionarMedico(datosAgendarConsulta);
 
         var consulta = new  Consulta(null, medico, paciente, datosAgendarConsulta.fecha());
 
         consultaRepository.save(consulta);
+    }
+
+    private Medico seleccionarMedico(DatosAgendarConsulta datosAgendarConsulta) {
+        return null;
     }
 }
